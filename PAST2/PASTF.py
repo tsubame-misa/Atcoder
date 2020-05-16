c1bba060772afc5667352a810fd53350cce9d3df
Tsubame <watamisa08130505@outlook.jp>
2020-05-16 22:30:28 +0900

    heapq

---
+++ PAST2/PASTF.py
@@ -0,0 +1,16 @@
+n = int(input())
+# query = [list(map(int, input().split())) for i in range(n)]
+day = [[] for i in range(n)]
+for i in range(n):
+    a, b = list(map(int, input().split()))
+    day[a-1].append(b)
+
+data = []
+heapq.heapify(data)
+a = 0
+for i in range(n):
+    for j in range(len(day[i])):
+        heapq.heappush(data, day[i][j]*-1)
+    a += heapq.heappop(data)*-1
+    print(a)
+    # print(data)
